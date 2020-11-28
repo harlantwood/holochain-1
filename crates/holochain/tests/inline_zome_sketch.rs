417da9012b1e8e3efea3e39915bec67272b43565
Michael Dougherty <maackle.d@gmail.com>
2020-11-27 18:15:58 -0800

    Flatten imports

--- crates/holochain/tests/inline_zome_sketch.rs
+++ crates/holochain/tests/inline_zome_sketch.rs
@@ -1,12 +1,12 @@
 use futures::StreamExt;
 use hdk3::prelude::*;
-use holochain::{conductor::Conductor, core::ribosome::ZomeCallInvocation};
+use holochain::conductor::Conductor;
+use holochain::core::ribosome::ZomeCallInvocation;
 use holochain_keystore::KeystoreSender;
 use holochain_state::test_utils::test_environments;
-use holochain_types::{
-    app::InstalledCell,
-    dna::{zome::inline_zome::InlineZome, DnaFile},
-};
+use holochain_types::app::InstalledCell;
+use holochain_types::dna::zome::inline_zome::InlineZome;
+use holochain_types::dna::DnaFile;
 use holochain_zome_types::element::ElementEntry;
 use unwrap_to::unwrap_to;
 
